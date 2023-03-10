d559c41768fdc9f1694bfa8cb5405eb011847d0d
QodirovDostonvek <kadirovdoston777@gmail.com>
2023-03-10 16:02:08 +0500

    test
    test uchun emas oddiy proba

---
+++ main2.py
@@ -0,0 +1,15 @@
+
+talabalar = ["hasan", "husan", "olim", "botir"]
+baholangan_talabalar = {}
+while talabalar:
+    talaba = talabalar.pop()
+    baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+    print(f"{talaba.title()} baholandi")
+    baholangan_talabalar[talaba] = int(baho)
+
+
+
+# masala
+# talabalar1 = {
+#     'id12345': 'ali hakimov',
+#     }
